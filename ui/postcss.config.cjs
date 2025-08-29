306a070ac09361e646203815f355a35c7dbf9559
SoFMeRight <kai.hamil@gmail.com>
2025-08-29 17:15:50 +0000

    Fix proper cjs format.

--- ui/postcss.config.cjs
+++ ui/postcss.config.cjs
@@ -1,6 +1,7 @@
-export default {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {}
-    }
-  };
+// ui/postcss.config.cjs
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -1,6 +1,10 @@
+// ui/tailwind.config.cjs
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: { brand: "#74ecbe" }
+    }
+  },
+  plugins: []
 };
